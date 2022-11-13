5f89f5c0a05e6fced159e6a4aad7f70478a1e8d3
JP <jpniznhy@hotmail.com>
2022-11-13 12:08:18 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from paint import *
 from algorisme import *
 from read_write import *
-# from algorisme2 import *
-# from algorisme4 import *
 
 
 def main():
-    ntest: int = 0.
+    ntest: int = 0
     for test in ['priv_testcase0.def', 'priv_testcase1.def', 'priv_testcase2.def', 'priv_testcase3.def']:
-        ldri, lpin = read_input(test)
+        ldri, lpin = read_input(test, './testCases/')
         paths = gen(lpin, ldri)
-        write_output(paths, './output' + str(ntest) + '.def')
+        write_output(paths, './solCases/output' + str(ntest) + '.def')
         ntest += 1
-        # add_nodes_and_edges(paths)
-        # paint_nodes_and_show()
+        add_nodes_and_edges(paths)
+        paint_nodes_and_show('./solCases/output_img' + str(ntest) + '.png')
 
 
 if __name__ == '__main__':
