38ce23d53e71552ee5c1423f5a4ef26e70196f2d
Benet Ramió i Comas <101699331+benetraco@users.noreply.github.com>
2022-11-13 13:22:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from paint import *
-from algorisme import *
+from algorithm import *
 from read_write import *
 
 
@@ -11,8 +11,8 @@ the paths as the desired format in an output file and creates an image visualizi
 
 def main():
     ntest: int = 0
-    for test in ['priv_testcase0.def', 'priv_testcase1.def', 'priv_testcase2.def', 'priv_testcase3.def', 'priv_testcase3.def']:
-        ldri, lpin = read_input(test, './testCases/')
+    for test in ['priv_testcase0.def', 'priv_testcase1.def', 'priv_testcase2.def', 'priv_testcase3.def']:
+        ldri, lpin = read_input(test)
         paths = gen(lpin, ldri)
         write_output(paths, 'f_output' + str(ntest) + '.def')
         add_nodes_and_edges(paths)
